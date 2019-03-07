192e1f61e41a0d27c0cbde69a4043bcc5789b3c9
webengineer0606 <webengineer0606@gmail.com>
2019-03-07 05:41:35 -0500

    Add react-loading-spin and fix linting for homepage

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 
 const HomePage = () => {
   return (
     <div className="m-4">
-      <h2>Buhaescu Gabriel's sample test application</h2>
+      <h2>{"Buhaescu Gabriel's sample test application"}</h2>
     </div>
   );
 };
