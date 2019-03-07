e2a9f087b53e810880ad23c77d23d797cf83a69a
webengineer0606 <webengineer0606@gmail.com>
2019-03-07 12:50:01 -0500

    Remove unecessary comments

--- src/reducers/postsReducer.js
+++ src/reducers/postsReducer.js
@@ -16,24 +16,16 @@ export default function postsReducer(state = initialState.posts, action) {
   
   switch (action.type) {
     case FETCH_POSTS_REQUEST:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {isLoading: true});
 
     case FETCH_POSTS_SUCCESS:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {items: action.postsData, filteredItems: action.postsData.slice(), isLoading:false});
 
     case FETCH_POSTS_ERROR:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {isError: true, isLoading:false});
 
     case FILTER_POSTS: 
       {
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
         const filteredItems = state.items.filter(({title}) => {
           return title.toLowerCase().includes(action.filterText.toLowerCase())
         });
--- src/reducers/usersReducer.js
+++ src/reducers/usersReducer.js
@@ -14,18 +14,12 @@ import initialState from './initialState';
 export default function usersReducer(state = initialState.users, action) {
   switch (action.type) {
     case FETCH_USERS_REQUEST:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {isLoading: true});
 
     case FETCH_USERS_SUCCESS:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {items: action.userData, isLoading:false});
 
     case FETCH_USERS_ERROR:
-      // For this example, just simulating a save by changing date modified.
-      // In a real app using Redux, you might use redux-thunk and handle the async call in fuelSavingsActions.js
       return objectAssign({}, state, {isError: true, isLoading:false});
 
     default:
