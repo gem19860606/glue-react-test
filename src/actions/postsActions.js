8e25d3153e10b0e36140ff7bb821562a4f268fa1
webengineer0606 <webengineer0606@gmail.com>
2019-03-07 07:44:18 -0500

    Done with postActions

--- src/actions/postsActions.js
+++ src/actions/postsActions.js
@@ -1,13 +1,24 @@
 import * as types from '../constants/actionTypes';
-
+import { POSTS_API_URL } from '../constants/apiUrl';
+import axios from 'axios';
 
 // example of a thunk using the redux-thunk middleware
 export function fetchPosts() {
   return function (dispatch) {
     // thunks allow for pre-processing actions, calling apis, and dispatching multiple actions
     // in this case at this point we could call a service that would persist the fuel savings
+    axios.get(POSTS_API_URL).then(({data}) => {
+      dispatch({
+        type: types.FETCH_POSTS_SUCCESS,
+        postsData: data,
+      });
+    }).catch(() => {
+      dispatch({
+        type: types.FETCH_USERS_ERROR,
+      });
+    });
     return dispatch({
-      type: types.FETCH_POSTS
+      type: types.FETCH_POSTS_REQUEST
     });
   };
 }
